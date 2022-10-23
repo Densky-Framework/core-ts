b8e32d7f068d7bb576ffbfc5e7ba549dcd8ab30c
Brayan-724 <bjqf0724@gmail.com>
2022-10-22 21:39:09 -0700

    chore(example) Update with current features

--- example_server/src/ws/_connect.ts
+++ example_server/src/ws/_connect.ts
@@ -1,3 +1,5 @@
-export default function connect() {
-  console.log("Socket is connected")
+import {SocketCtx, Socket} from "dusky"
+
+export default function connect(_: SocketCtx, socket: Socket) {
+  console.log("Socket is connected:", socket.id)
 }
