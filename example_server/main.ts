0c49301a3d098b48cb1cdd8fd67308febb630e2f
Brayan-724 <bjqf0724@gmail.com>
2022-10-30 10:40:05 -0700

    fix(example) Deno cwd resolve

--- example_server/main.ts
+++ example_server/main.ts
@@ -1,5 +1,9 @@
 import { Server } from "densky";
-import requestHandler from "./.densky/main.ts";
+
+const pathname = new URL(import.meta.resolve("./")).pathname;
+Deno.chdir(pathname);
+
+const { default: requestHandler } = await import("./.densky/main.ts");
 
 const server = new Server({ port: 8000, verbose: true }, requestHandler);
 
