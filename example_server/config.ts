b677b1a856cdc7abdfe49d774d8ae5973306095b
Brayan-724 <bjqf0724@gmail.com>
2022-10-29 18:58:05 -0700

    chore(example) Add static folder to config

--- example_server/config.ts
+++ example_server/config.ts
@@ -1,9 +1,11 @@
-import { CompileOptions } from "dusky/compiler.ts";
+import { CompileOptions } from "densky/compiler.ts";
 
 export const routesPath = "src/routes";
 
 export const compileOptions: CompileOptions = {
   routesPath: "src/routes",
   wsPath: "src/ws",
+  staticPath: "src/static",
+  staticPrefix: "/static",
   verbose: true,
 };
